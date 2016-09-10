458c1956fe6f4af9ce00c443c8f82d7888a2fbd7
asdfa <asdfakf>
2016-09-10 09:46:03 -0600

    Lab 2 - without setting time

---
+++ Lab2_clock/real_time_clock/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d src/platform.o: ../src/platform.c \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xparameters.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xil_cache.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/mb_interface.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xbasic_types.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xil_types.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xparameters.h \
+  ../src/platform_config.h
+
+../../real_time_clock_bsp_0/microblaze_0/include/xparameters.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xil_cache.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xil_types.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
---
+++ Lab2_clock/real_time_clock/Debug/src/real_time_clock.d
@@ -0,0 +1,39 @@
+src/real_time_clock.d src/real_time_clock.o: ../src/real_time_clock.c \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xgpio.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xil_types.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xil_assert.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xstatus.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xbasic_types.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xgpio_l.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xil_io.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/mb_interface.h \
+  ../src/platform.h ../src/platform_config.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/mb_interface.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xintc_l.h \
+  ../../real_time_clock_bsp_0/microblaze_0/include/xparameters.h
+
+../../real_time_clock_bsp_0/microblaze_0/include/xgpio.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xil_types.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xstatus.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xgpio_l.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xil_io.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/mb_interface.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xintc_l.h:
+
+../../real_time_clock_bsp_0/microblaze_0/include/xparameters.h:
