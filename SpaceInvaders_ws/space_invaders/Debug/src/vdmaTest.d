c3e53f4cdf081ae960b5ca4956ad0b1cc166e437
DallonTG <asdfakf>
2016-09-21 18:22:40 -0600

    Not quite working...

---
+++ SpaceInvaders_ws/space_invaders/Debug/src/vdmaTest.d
@@ -0,0 +1,42 @@
+src/vdmaTest.d src/vdmaTest.o: ../src/vdmaTest.c ../src/platform.h \
+  ../src/platform_config.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xparameters.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xaxivdma.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xaxivdma_hw.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xil_types.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xil_io.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/mb_interface.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xbasic_types.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xaxivdma_i.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xdebug.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xstatus.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xil_assert.h \
+  C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xio.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xparameters.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xaxivdma.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xaxivdma_hw.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xil_types.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xil_io.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/mb_interface.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xbasic_types.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xaxivdma_i.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xdebug.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xstatus.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xil_assert.h:
+
+C:/Users/superman/Documents/ecen427/SpaceInvaders_ws/space_invaders_bsp/microblaze_0/include/xio.h:
