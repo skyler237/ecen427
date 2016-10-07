4f3477e4f6b6af562f84f713efbd07d1b6b4d7c1
Troy  Hinckley <superman@CB400-05.WORKGROUP>
2016-10-07 17:03:29 -0600

    Added control stuff (and new workspace)

---
+++ SpaceInvaders_ws2/space_invaders/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d src/platform.o: ../src/platform.c \
+  ../../space_invaders_bsp/microblaze_0/include/xparameters.h \
+  ../../space_invaders_bsp/microblaze_0/include/xil_cache.h \
+  ../../space_invaders_bsp/microblaze_0/include/mb_interface.h \
+  ../../space_invaders_bsp/microblaze_0/include/xbasic_types.h \
+  ../../space_invaders_bsp/microblaze_0/include/xil_types.h \
+  ../../space_invaders_bsp/microblaze_0/include/xparameters.h \
+  ../src/platform_config.h
+
+../../space_invaders_bsp/microblaze_0/include/xparameters.h:
+
+../../space_invaders_bsp/microblaze_0/include/xil_cache.h:
+
+../../space_invaders_bsp/microblaze_0/include/mb_interface.h:
+
+../../space_invaders_bsp/microblaze_0/include/xbasic_types.h:
+
+../../space_invaders_bsp/microblaze_0/include/xil_types.h:
+
+../../space_invaders_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
