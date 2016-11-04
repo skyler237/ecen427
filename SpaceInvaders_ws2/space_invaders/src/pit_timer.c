2fb63af0e0dc0d3f6043c824aa34f989320dbdef
skyler237 <skyler237@gmail.com>
2016-11-03 19:16:07 -0600

    Code cleanup (lab 6)

--- SpaceInvaders_ws2/space_invaders/src/pit_timer.c
+++ SpaceInvaders_ws2/space_invaders/src/pit_timer.c
@@ -13,66 +13,101 @@
 #define DELAY_COUNT_REG (XPAR_PITIMER_0_BASEADDR+8)	// Delay count register - holds the "top" value of the timer
 #define CONTROL_REG (XPAR_PITIMER_0_BASEADDR+4)		// Control register - controls timer settings
 
+// Default initial value for the timer
 #define TIMER_DEFAULT 1000000
 
-#define START_MASK 0x1
-#define INTERRUPT_MASK 0x2
-#define LOOP_MASK 0x4
+// Control masks
+#define START_MASK 0x1 			// Mask to enable/disable decrementing the counter
+#define INTERRUPT_MASK 0x2		// Mask to enable/disable interrupts
+#define LOOP_MASK 0x4			// Mask to enable/disable looping to the top when counter expires
 
+// Initialize the PIT
 void pit_timer_init(){
-	Xil_Out32(COUNT_REG, TIMER_DEFAULT);
-	Xil_Out32(DELAY_COUNT_REG, TIMER_DEFAULT);
-	Xil_Out32(CONTROL_REG, 0);
+	Xil_Out32(COUNT_REG, TIMER_DEFAULT); // Set the default value for the counter
+	Xil_Out32(DELAY_COUNT_REG, TIMER_DEFAULT); // Set the delay count register to default value
+	Xil_Out32(CONTROL_REG, 0); // Initialize control to zero (decrement, interrupts, and looping off)
 }
 
+// Starts the timer 
 void pit_timer_start(){
+	// Read out current settings
 	uint32_t settings = Xil_In32(CONTROL_REG);
+	// Add the start bit
 	settings |= START_MASK;
+	// Write back the control
 	Xil_Out32(CONTROL_REG, settings);
 }
 
+// Stops the timer
 void pit_timer_stop(){
+	// Read out current settings
 	uint32_t settings = Xil_In32(CONTROL_REG);
+	// Remove the start bit
 	settings &= ~START_MASK;
+	// Write back the control
 	Xil_Out32(CONTROL_REG, settings);
 }
 
+// Enables interrupts for the timer
 void pit_timer_enableInt(){
+	// Read out current settings
 	uint32_t settings = Xil_In32(CONTROL_REG);
+	// Add the interrupt bit
 	settings |= INTERRUPT_MASK;
+	// Write back the control
 	Xil_Out32(CONTROL_REG, settings);
 }
 
+// Disables interrupts for the timer
 void pit_timer_disableInt(){
+	// Read out current settings
 	uint32_t settings = Xil_In32(CONTROL_REG);
+	// Remove the interrupt bit
 	settings &= ~INTERRUPT_MASK;
+	// Write back the control
 	Xil_Out32(CONTROL_REG, settings);
 }
 
+// Enable looping 
 void pit_timer_loopOn(){
+	// Read out current settings
 	uint32_t settings = Xil_In32(CONTROL_REG);
+	// Add the loop bit
 	settings |= LOOP_MASK;
+	// Write back the control
 	Xil_Out32(CONTROL_REG, settings);
 }
 
+// Disable looping
 void pit_timer_loopOff(){
+	// Read out current settings
 	uint32_t settings = Xil_In32(CONTROL_REG);
+	// Remove the loop bit
 	settings &= ~LOOP_MASK;
+	// Write back the control
 	Xil_Out32(CONTROL_REG, settings);
 }
 
+// Set the timer value
 void pit_timer_setTimer(uint32_t timer_value){
+	// Set the delay count reg value
 	Xil_Out32(DELAY_COUNT_REG, timer_value);
 }
 
+// Returns the count register value
 uint32_t pit_timer_readCountReg(){
+	// Read the count reg value
 	return Xil_In32(COUNT_REG);
 }
 
+// Returns the dealy count register value
 uint32_t pit_timer_readDelayReg(){
+	// Read the delay count reg value
 	return Xil_In32(DELAY_COUNT_REG);
 }
 
+// Returns the control register value
 uint32_t pit_timer_readControlReg(){
+	// Read the control reg value
 	return Xil_In32(CONTROL_REG);
 }
