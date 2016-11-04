d8154b1dc97cb00f8f62df14ddb5b8907dd78a1e
skyler237 <skyler237@gmail.com>
2016-11-03 19:15:05 -0600

    Code cleanup (lab 6)

--- SpaceInvaders_ws2/space_invaders/src/pit_timer.h
+++ SpaceInvaders_ws2/space_invaders/src/pit_timer.h
@@ -10,26 +10,29 @@
 
 #include <stdint.h>
 
+// Initialize the PIT
 void pit_timer_init();
-
+// Starts the timer 
 void pit_timer_start();
-
+// Stops the timer
 void pit_timer_stop();
-
+// Enables interrupts for the timer
 void pit_timer_enableInt();
-
+// Disables interrupts for the timer
 void pit_timer_disableInt();
-
+// Enable looping 
 void pit_timer_loopOn();
-
+// Disable looping
 void pit_timer_loopOff();
-
+// Set the timer value
 void pit_timer_setTimer(uint32_t timer_value);
-
+// Returns the count register value
 uint32_t pit_timer_readCountReg();
-
+// Returns the dealy count register value
 uint32_t pit_timer_readDelayReg();
-
+// Returns the control register value
 uint32_t pit_timer_readControlReg();
+
+
 
 #endif /* PIT_TIMER_H_ */
